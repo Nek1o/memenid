40e25a71abb97444941ab7702373ab46038482d6
nekio <nikita.volchenkov1@gmail.com>
2021-06-17 18:34:57 +0300

    config header

---
+++ src/config/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DEFAULT_PORT 8000
+#define DEFAULT_HOST "0.0.0.0"
+
+#define DOC "A simple C gemini server"
+#define ARGS_DOC "TODO"
+
+#endif // !CONFIG_H
