37f18f5be1d1f1e7546e7e1cacb1d5fa5f6575ed
Nek1o <nikita.volchenkov1@gmail.com>
2021-06-19 00:49:28 +0300

    send_all instead of write and test sigint handler

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+#include <signal.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -13,9 +14,22 @@
 #include "config.h"
 #include "args/server_args.h"
 #include "network/socket.h"
+#include "signals/signals.h"
+
+// main while loop condition variable
+static volatile sig_atomic_t keep_running = 1;
+
+// sigint_handler inverts the main while loop condition variable
+// and therefore stops the program
+void sigint_handler(int signum)
+{
+    write(1, "Graceful shutdown", 17);
+    keep_running = 0;
+}
 
 int main(int argc, char **argv)
 {
+    signal(SIGINT, sigint_handler);
     // TODO signals handling for proper socket closing
     // fuser -k <port>/tcp
 
@@ -24,7 +38,7 @@ int main(int argc, char **argv)
     int sock_fd = configure_tcp_socket(args.host, args.port);
 
     int conn_fd = -1;
-    while (1)
+    while (keep_running)
     {
         char conn_buff[MAX_URL_SIZE];
         if ((conn_fd = accept(sock_fd, (struct sockaddr *)NULL, NULL)) == -1)
@@ -45,16 +59,18 @@ int main(int argc, char **argv)
             break;
         case 0:
             conn_buff[read_res] = '\0';
-            printf("Successfully received a request %s", conn_buff);
+            printf("Read returned 0");
             break;
         }
-
-        if ((write(conn_fd, conn_buff, strlen(conn_buff))) == -1)
+        
+        int *conn_len;
+        *conn_len = strlen(conn_buff);
+        if ((send_all(conn_fd, conn_buff, conn_len)) == -1)
         {
             perror("An error occurred when trying to write stuff to connection");
             continue;
         }
-
+        
         // printf("DATA: %s", conn_buff);
         memset(conn_buff, 0, MAX_URL_SIZE);
         close(conn_fd);
---
+++ src/signals/signals.h
@@ -0,0 +1,8 @@
+#ifndef SIGNALS_H
+#define SIGNALS_H
+
+#include <signal.h>
+
+void sigint_handler(int);
+
+#endif // !SIGNALS_H
