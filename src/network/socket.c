d54c9bd4625d3e8ab52272723e5bd3710ec7f8e7
Nek1o <nikita.volchenkov1@gmail.com>
2021-06-19 00:49:02 +0300

    send_all func

--- src/network/socket.c
+++ src/network/socket.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "socket.h"
 #include "../config.h"
@@ -15,7 +16,7 @@ int configure_tcp_socket(in_addr_t host, int port)
         perror("Socket openning failed");
         exit(EXIT_FAILURE);
     }
-    
+
     struct sockaddr_in serv_addr;
     // preallocating the memory
     memset(&serv_addr, '0', sizeof(serv_addr));
@@ -33,6 +34,7 @@ int configure_tcp_socket(in_addr_t host, int port)
     if (bind(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
     {
         perror("Socket binding failed");
+        close(sock_fd);
         exit(EXIT_FAILURE);
     }
 
@@ -41,8 +43,33 @@ int configure_tcp_socket(in_addr_t host, int port)
         char *err_buff;
         sprintf(err_buff, "Could not listen on port %d", port);
         perror(err_buff);
+        close(sock_fd);
         exit(EXIT_FAILURE);
     }
 
     return sock_fd;
+}
+
+// send_all sends all the data from buff via socket fd,
+// len initially is a length of buff aka the number of bytes to be sent
+// then len is set to the number of bytes actually sent.
+int send_all(int fd, char *buff, int *len)
+{
+    int total = 0; // bytes send
+    int bytes_left = *len;
+    int n;
+
+    while (total < *len)
+    {
+        n = send(fd, buff + total, bytes_left, 0);
+        if (n == -1)
+        {
+            perror("An error occurred during sending data via socket");
+            break;
+        }
+        total += n;
+        bytes_left -= n;
+    }
+    *len = total;
+    return n == -1 ? -1 : 0; // 0 on success, -1 on error
 }
--- src/network/socket.h
+++ src/network/socket.h
@@ -3,6 +3,12 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 
+// configure_tcp_socket creates a tcp socket socket according to config and cli args.
+// Socket is bound and listening ready to accept connections.
 int configure_tcp_socket(in_addr_t, int);
+
+// send_all sends all the data from buff via socket fd
+// len is set to the number of bytes sent
+int send_all(int, char *, int *);
 
 #endif // !SOCKET_H
